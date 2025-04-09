bc1977e3b599162373016930e7c107f80bf4f2d1
Vaistaya10 <vaistayer10@gmail.com>
2025-04-09 11:58:03 -0500

    integracion de la API de DNI

---
+++ app/controllers/consultaDni.php
@@ -0,0 +1,35 @@
+<?php
+// consultaDni.php
+
+// Verifica que se reciba el parámetro 'dni'
+if (!isset($_GET['dni'])) {
+    header('Content-Type: application/json');
+    echo json_encode(["error" => "No se ha especificado el DNI"]);
+    exit;
+}
+
+$token = 'apis-token-14104.g67iElklbuoH8v51T7cOgcVhrMLGicKv';
+$dni = $_GET['dni'];
+
+// Iniciar llamada a la API
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.apis.net.pe/v2/reniec/dni?numero=' . $dni,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYPEER => 0,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 2,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Referer: https://apis.net.pe/consulta-dni-api',
+    'Authorization: Bearer ' . $token
+  ),
+));
+$response = curl_exec($curl);
+curl_close($curl);
+
+header('Content-Type: application/json');
+echo $response;
+?>
